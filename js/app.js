ba53888038ba677ea38d182d5b480be24f0a6667
joonon2 <x2junwon@naver.com>
2022-02-17 10:32:11 +0900

    submit process added

--- js/app.js
+++ js/app.js
@@ -2,11 +2,18 @@ const greeting = document.getElementById("greeting")
 const nameInput = document.getElementById("name-input")
 const nameForm = document.getElementById("name-form")
 
-function changeGreeting(event) {
+//이름이 입력되면 폼 숨기기
+function loginSubmit(event) {
     event.preventDefault();
     const username = nameInput.value;
-    greeting.innerText = "Hello" + username;
+    nameForm.classList.add("hidden")
+    changeGreeting(username)
 }
 
+//이름 입력되면 이름반영해서 인사하기
+function changeGreeting(name) {
+    greeting.innerText = "Hello" + name;
+    nameInput.value=""
+}
 
-nameForm.addEventListener("submit",changeGreeting)
+nameForm.addEventListener("submit", loginSubmit);
